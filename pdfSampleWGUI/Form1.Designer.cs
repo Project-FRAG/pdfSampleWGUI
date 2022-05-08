d735250624734eddc8b8c1e8b2b6e58e01f70344
Shunsuke Mochizuki <mochizuki.shunsuke@wings-consulting.jp>
2022-05-08 20:22:48 +0900

    プロジェクト ファイルを追加します。

---
+++ pdfSampleWGUI/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace pdfSampleWGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 70);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(796, 631);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(148, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TextFragment一覧";
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(796, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "読み込み";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(796, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "書き込み";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 701);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ pdfSampleWGUI/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pdfSampleWGUI
+{
+    public partial class Form1 : Form
+    {
+        Aspose.Pdf.Document pdf;
+        Aspose.Pdf.Text.TextFragmentCollection fragments;
+        List<TextBox> boxes;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            if(openFileDialog1.FileName != "" && openFileDialog1.FileName.EndsWith(".pdf"))
+            {
+                pdf = new Aspose.Pdf.Document(openFileDialog1.FileName);
+                var textFragmentAbsorberAddress = new Aspose.Pdf.Text.TextFragmentAbsorber();
+                pdf.Pages[1].Accept(textFragmentAbsorberAddress);
+                fragments = textFragmentAbsorberAddress.TextFragments;
+                int i = 0;
+                boxes = new List<TextBox>();
+                foreach(var f in fragments)
+                {
+                    var box = new TextBox() { Text = f.Text };
+                    var under = new CheckBox() { Text = "下線",Checked = f.TextState.Underline, Name = i.ToString()};
+                    var strikeout = new CheckBox() { Text = "打消し", Checked = f.TextState.StrikeOut, Name = i.ToString() };
+                    this.Controls.Add(box);
+                    flowLayoutPanel1.Controls.Add(box);
+                    under.CheckedChanged += Under_CheckedChanged;
+                    flowLayoutPanel1.Controls.Add(under);
+                    strikeout.CheckedChanged += Strikeout_CheckedChanged;
+                    flowLayoutPanel1.Controls.Add(strikeout);
+                    boxes.Add(box);
+                    i++;
+                }
+                button1.Enabled = false;
+            }
+            else
+            {
+                MessageBox.Show("ファイル名が不正です", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Under_CheckedChanged(object sender, EventArgs e)
+        {
+            var checkbox = (CheckBox)sender;
+            var frag = fragments.ElementAt(Int32.Parse(checkbox.Name));
+            frag.TextState.Underline = checkbox.Checked;
+        }
+
+        private void Strikeout_CheckedChanged(object sender, EventArgs e)
+        {
+            var checkbox = (CheckBox)sender;
+            var frag = fragments.ElementAt(Int32.Parse(checkbox.Name));
+            frag.TextState.StrikeOut = checkbox.Checked;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.FileName = openFileDialog1.FileName.Replace(".pdf","_mod.pdf");
+            saveFileDialog1.ShowDialog();
+            if(saveFileDialog1.FileName != "" && saveFileDialog1.FileName.EndsWith(".pdf"))
+            {
+                try
+                {
+                    for(int i = 0; i < boxes.Count; i++)
+                    {
+                        var f = fragments.ElementAt(i);
+                        f.Text = boxes.ElementAt(i).Text;
+                    }
+                    pdf.Save(saveFileDialog1.FileName);
+                    MessageBox.Show("書き込み成功", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    this.Close();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("対応していない文字が含まれています", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("ファイル名が不正です", "", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        
+    }
+}
